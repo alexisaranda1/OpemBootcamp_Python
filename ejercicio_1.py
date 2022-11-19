0daf3732f220c74791c15bc7f027b223d5415c43
alexisaranda1 <arandajaaraalexis@gmail.com>
2022-11-19 20:21:27 -0300

    segundo ejercicio

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,5 +1,6 @@
 # Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
 
-mensaje = " Hola mundo! "
 
-print(mensaje)
+mensaje = " Hola mundo! " # declaro una variable y le asigno una cadana de texto. 
+
+print(mensaje) # muestro la variable con la funcion print.
---
+++ ejercicio_2.py
@@ -0,0 +1,13 @@
+# Modifica la variable del anterior ejercicio en la consola de python 
+# y después muestrala por consola para ver la modificación de la variable.
+
+
+mensaje = " Hola mundo! " # declaro una variable y le asigno una cadana de texto. 
+
+print(mensaje) # muestro la variable con la funcion print.
+
+mensaje = " chau mundo! " # modifico la varable...
+
+print(mensaje) # vuelvo a mostrar la variable ahor modificada.
+
+
