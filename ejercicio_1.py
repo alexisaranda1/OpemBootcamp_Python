70b5155ad53a22a3bd63ce09aff9ce9750d06ef2
alexisaranda1 <arandajaaraalexis@gmail.com>
2022-11-19 20:22:48 -0300

    agregue comentarios

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,6 +1,5 @@
 # Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
 
-
 mensaje = " Hola mundo! " # declaro una variable y le asigno una cadana de texto. 
 
 print(mensaje) # muestro la variable con la funcion print.
